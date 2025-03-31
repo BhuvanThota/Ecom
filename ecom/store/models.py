d6c0441805076693078bd8674c5cf991a6cc6862
BhuvanThota <teacherb3337@gmail.com>
2025-03-31 15:35:23 +0530

    Login updated

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -38,6 +38,10 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, default=1, null=True)
     description = models.TextField(default = '', blank=True, null= True)
     image = models.ImageField(upload_to='uploads/product/')
+    # Adding Sale Stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default = 0, max_digits=10, decimal_places=2)
+    
 
     def __str__(self):
         return self.name
